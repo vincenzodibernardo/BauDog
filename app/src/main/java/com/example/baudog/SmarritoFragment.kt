f4b1de7f62d267100e721fc5e2ecc7e98e0549a6
Vincenzo Di Bernardo <vinc.96@alice.it>
2020-01-10 03:04:47 +0100

    In HomeFragment.kt -> eliminato codice attualmente inutile e creato il funzionamento dei bottoni Smarrimento e Ritrovamento. Creato TrovatoFragment, SmarritoFragment, aggiornato background bottone home.
    prova bottoni xml per fragment smarrito e provato

--- app/src/main/java/com/example/baudog/SmarritoFragment.kt
+++ app/src/main/java/com/example/baudog/SmarritoFragment.kt
@@ -7,8 +7,9 @@ import android.support.v4.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-\import androidx.navigation.Navigation
+import androidx.navigation.Navigation
 import kotlinx.android.synthetic.main.fragment_home.*
+
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -18,41 +19,26 @@ private const val ARG_PARAM2 = "param2"
 /**
  * A simple [Fragment] subclass.
  * Activities that contain this fragment must implement the
- * [HomeFragment.OnFragmentInteractionListener] interface
+ * [SmarritoFragment.OnFragmentInteractionListener] interface
  * to handle interaction events.
- * Use the [HomeFragment.newInstance] factory method to
+ * Use the [SmarritoFragment.newInstance] factory method to
  * create an instance of this fragment.
  *
  */
-class HomeFragment : Fragment() {
+class SmarritoFragment : Fragment() {
     // TODO: Rename and change types of parameters
     private var param1: String? = null
     private var param2: String? = null
 
 
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
-    }
-
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home, container, false)
+        return inflater.inflate(R.layout.fragment_smarrito, container, false)
     }
-
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        super.onViewCreated(view, savedInstanceState)
-        Bottone_Smarrimento.setOnClickListener { Navigation.findNavController(view).navigate(R.id.action_homeFragment_to_smarritoFragment) }
-        Bottone_Ritrovamento.setOnClickListener { Navigation.findNavController(view).navigate(R.id.action_homeFragment_to_trovatoFragment) }
-    }
-
 
 
 
